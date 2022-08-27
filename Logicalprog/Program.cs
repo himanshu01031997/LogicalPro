998eea501bd0caeebc5abe8f12cb65a6276e0f70
himanshu01031997 <hs010397@gmail.com>
2022-08-27 16:48:53 +0530

    [himanshu]Add:added welcome message

---
+++ Logicalprog/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logicalprog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to logical program");
+        }
+    }
+}
