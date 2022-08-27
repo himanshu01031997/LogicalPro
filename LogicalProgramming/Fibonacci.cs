c660f4fe7c306f067db6b6558350e3849a2a70eb
himanshu01031997 <hs010397@gmail.com>
2022-08-27 21:09:42 +0530

    [himanshu]add:fibonacci series

---
+++ LogicalProgramming/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class Fibonacci
+    {
+        int firstNum = 0, secondNum = 1, thirdNum = 0, i;
+
+        public void Fibona()
+        {
+            Console.Write("\n");
+            Console.Write("To Print Febonacci Series:\n");
+            Console.Write("----------------------------------------------\n");
+            Console.Write("\n");
+            Console.WriteLine("enter the Number to Print FibonacciSeries : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Febonacci Series is : \n" + firstNum + "\n" + secondNum);
+            while (num >= 2)
+            {
+                thirdNum = firstNum + secondNum;
+                Console.WriteLine(thirdNum + "");
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                num--;
+            }
+        }
+    }
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Select Your choice \n 1.fibonacci \n");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Fibona();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
