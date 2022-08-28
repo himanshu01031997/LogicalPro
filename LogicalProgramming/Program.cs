ac5de17d595de62557b858f92090f071c5b0cdb0
himanshu01031997 <hs010397@gmail.com>
2022-08-28 10:25:42 +0530

    [himanshu]Add:added reverse no

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Select Your choice \n1.fibonacci \n2. Prime no\n3.Perfect no\n");
+            Console.WriteLine("Please Select Your choice \n1.fibonacci \n2. Prime no\n3.Perfect no\n4.Reverse no\n");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -20,6 +20,10 @@
                 case 3:
                     PerfectNo perfect = new PerfectNo();
                     perfect.checkperfect();
+                    break;
+                case 4:
+                    Reverse reverse = new Reverse();
+                    reverse.CheckReverse();
                     break;
                 default:
                     break;
---
+++ LogicalProgramming/Reverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class Reverse
+    {
+        int rem,count =0;
+
+        public void CheckReverse()
+        {
+            Console.Write("\n");
+            Console.Write("To Print Reverse no:\n");
+            Console.Write("----------------------------------------------\n");
+            Console.Write("\n");
+            Console.WriteLine("enter the Number to Print Reverse no : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while(num!=0)
+            {
+                int rem = num % 10;
+                count = count * 10 + rem;
+                num = num / 10;
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
