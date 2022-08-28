ab49e5230bc6ac8346f39da1e5284337a602ade3
himanshu01031997 <hs010397@gmail.com>
2022-08-28 09:38:11 +0530

    [himanshu]Add:perfect no added

---
+++ LogicalProgramming/PerfectNo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PerfectNo
+    {
+        int rem, sum = 0, i;
+        public void checkperfect()
+        {
+            Console.Write("\n");
+            Console.Write("To Print Perfect no:\n");
+            Console.Write("----------------------------------------------\n");
+            Console.Write("\n");
+            Console.WriteLine("enter the Number to Print perfect no : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; (i <= num / 2); i++)
+            {
+                rem = num % i;
+                if (rem == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("no is perfect no");
+            }
+            else
+            {
+                Console.WriteLine("no is not a Pefect no ");
+            }
+        }
+    }
+}
--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Select Your choice \n 1.fibonacci \n2. Prime no\n");
+            Console.WriteLine("Please Select Your choice \n1.fibonacci \n2. Prime no\n3.Perfect no\n");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -16,6 +16,10 @@
                 case 2: 
                     PrimeNo prime=new PrimeNo();
                     prime.checkprime();
+                    break;
+                case 3:
+                    PerfectNo perfect = new PerfectNo();
+                    perfect.checkperfect();
                     break;
                 default:
                     break;
